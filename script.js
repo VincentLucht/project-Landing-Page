0b39a8faec3df94ef99c44b30e01216216f62d32
Vincent <vincentlucht04@gmail.com>
2023-12-05 21:39:53 +0100

    Syncing

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("container-1-button").addEventListener("click", function() {
+    alert("This button will soon work!");
+});
+
+document.getElementById("view-button-div").addEventListener("click", function() {
+    alert("This button will also work soon!")
+})
